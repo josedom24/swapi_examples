014a57545dd724228ed057136c6133e4699a147d
josedom24 <josedom24@gmail.com>
2018-04-02 18:42:20 +0200

    swapi

--- flask2/app.py
+++ flask2/app.py
@@ -7,20 +7,21 @@ URL_BASE="https://swapi.co/api/"
 def inicio():
     return render_template("index.html")
 
-@app.route('/<tipo>')
+@app.route('/<string:tipo>')
 def controlador(tipo):
 	info=convtipo(tipo)
 	r=requests.get(URL_BASE+info["url"])
 	if r.status_code == 200:
 		doc = r.json()    
-		return render_template("peliculas.html",datos=doc["results"],info=info)
+		return render_template("list.html",datos=doc["results"],info=info)
 			
-@app.route('/peliculas/<int:id>')
-def peliculas_detalle(id):
-	r=requests.get(URL_BASE+'films/%d'%id)
+@app.route('/<string:tipo>/<int:id>')
+def detalle(tipo,id):
+	info=convtipo(tipo)
+	r=requests.get(URL_BASE+info["url"]+'/%d'%id)
 	if r.status_code == 200:
 		doc = r.json()    
-		return render_template("peliculas_detalle.html",datos=doc,id=id)
+		return render_template("detalle.html",datos=doc,id=id)
 
 @app.route('/peliculas/<int:id>/personajes')
 def peliculas_personajes(id):
@@ -56,16 +57,32 @@ def peliculas_planetas(id):
 def convtipo(tipo):
 	if tipo=="peliculas":
 		info={"url":"films","nombre":"title"}
+		detalle=(("Episodio:","episode_id"),("Nombre:","title"),("Como empeza:","opening_crawl"),("Director:","director"),("Productor:","producer"),("Fecha de lanzamiento:","realease_date"))
+		listas=(("Personajes:","characters"),("Planetas:","planets"),("Naves:","starships"),("Vehiculos:","vehicules"),("Especies:","species"))
+
 	elif tipo=="planetas":
 		info={"url":"planets","nombre":"name"}
+		detalles=()
+		listas=()
 	elif tipo=="vehiculos":
 		info={"url":"vehicles","nombre":"name"}
+		detalles=()
+		listas=()
 	elif tipo=="naves":
 		info={"url":"starships","nombre":"name"}
+		detalles=()
+		listas=()
 	elif tipo=="personajes":
 		info={"url":"people","nombre":"name"}
+		detalles=()
+		listas=()
 	elif tipo=="especies":
 		info={"url":"species","nombre":"name"}	
+		detalles=()
+		listas=()
+	info["tipo"]=tipo
+	info["detalle"]=detalle
+	info["listas"]=listas
 	return info
 
 if __name__ == '__main__':
