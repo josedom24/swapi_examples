654f20494ba1256487235784c0cc9ddc3130c5ac
josedom24 <josedom24@gmail.com>
2018-04-02 20:11:07 +0200

    swapi

--- flask2/app.py
+++ flask2/app.py
@@ -20,70 +20,74 @@ def detalle(tipo,id):
 	info=convtipo(tipo)
 	r=requests.get(URL_BASE+info["url"]+'/%d'%id)
 	if r.status_code == 200:
-		doc = r.json()    
-		return render_template("detalle.html",datos=doc,id=id)
-
-@app.route('/peliculas/<int:id>/personajes')
-def peliculas_personajes(id):
-	r=requests.get(URL_BASE+'films/%d'%id)
-	if r.status_code == 200:
-		doc = r.json()  
-		url_personajes=doc["characters"]
-		personajes=[]
-		for url_personaje in url_personajes:
-			r=requests.get(url_personaje)
+		doc = r.json()
+		if tipo=="personajes":
+			info2=convtipo("planetas")
+			r=requests.get(doc["homeworld"])
 			if r.status_code == 200:
 				doc2=r.json()
-				personajes.append(doc2["name"])
+				nombre=doc2[info2["nombre"]]
+			url=conv_urls([doc["homeworld"]],"planetas")[0]
+			doc["homeworld"]='<a href="%s">%s</a>'%(url,nombre)
+		return render_template("detalle.html",datos=doc,id=id,info=info)
 
-		return render_template("lista.html",datos=zip(personajes,url_personajes),id=id,title=doc["title"],subtitle="Personajes",url_return="peliculas_detalle")
+@app.route('/<string:tipo1>/<int:id>/<string:tipo2>')
+def datos(tipo1,id,tipo2):
+	info1=convtipo(tipo1)
+	info2=convtipo(tipo2)
+	r=requests.get(URL_BASE+info1["url"]+'/%d'%id)
+	if r.status_code == 200:
+		doc = r.json()  
+		urls=doc[info2["dato"]]
+		datos=[]
+		for url in urls:
+			r=requests.get(url)
+			if r.status_code == 200:
+				doc2=r.json()
+				datos.append(doc2[info2["nombre"]])
+		urls=conv_urls(urls,tipo2)
+		return render_template("lista.html",datos=zip(datos,urls),id=id,title=doc[info1["nombre"]],subtitle=info2["titulo"],tipo1=tipo1,tipo2=tipo2)
     
-@app.route('/peliculas/<int:id>/planetas')
-def peliculas_planetas(id):
-	r=requests.get(URL_BASE+'films/%d'%id)
-	if r.status_code == 200:
-		doc = r.json()  
-		url_planetas=doc["planets"]
-		planetas=[]
-		for url_personaje in url_planetas:
-			r=requests.get(url_personaje)
-			if r.status_code == 200:
-				doc2=r.json()
-				planetas.append(doc2["name"])
-
-		return render_template("lista.html",datos=zip(planetas,url_planetas),id=id,title=doc["title"],subtitle="Planetas",url_return="peliculas_detalle")
-
 
 def convtipo(tipo):
 	if tipo=="peliculas":
-		info={"url":"films","nombre":"title"}
-		detalle=(("Episodio:","episode_id"),("Nombre:","title"),("Como empeza:","opening_crawl"),("Director:","director"),("Productor:","producer"),("Fecha de lanzamiento:","realease_date"))
-		listas=(("Personajes:","characters"),("Planetas:","planets"),("Naves:","starships"),("Vehiculos:","vehicules"),("Especies:","species"))
+		info={"url":"films","nombre":"title","titulo":"Películas","dato":"films"}
+		detalle=(("Episodio:","episode_id"),("Nombre:","title"),("Como empeza:","opening_crawl"),("Director:","director"),("Productor:","producer"),("Fecha de lanzamiento:","release_date"))
+		listas=("Personajes","Planetas","Naves","Vehiculos","Especies")
 
 	elif tipo=="planetas":
-		info={"url":"planets","nombre":"name"}
-		detalles=()
+		info={"url":"planets","nombre":"name","titulo":"Planetas","dato":"planets"}
+		detalle=("Periodo de rotación:":"rotation_period")
 		listas=()
 	elif tipo=="vehiculos":
-		info={"url":"vehicles","nombre":"name"}
-		detalles=()
+		info={"url":"vehicles","nombre":"name","titulo":"Vehículos","dato":"vehicles"}
+		detalle=()
 		listas=()
 	elif tipo=="naves":
-		info={"url":"starships","nombre":"name"}
-		detalles=()
+		info={"url":"starships","nombre":"name","titulo":"Naves","dato":"starships"}
+		detalle=()
 		listas=()
 	elif tipo=="personajes":
-		info={"url":"people","nombre":"name"}
-		detalles=()
-		listas=()
+		info={"url":"people","nombre":"name","titulo":"Personajes","dato":"characters"}
+		detalle=(("Nombre:","name"),("Altura:","height"),("Peso:","mass"),("Color del pelo:","hair_color"),("Color de la mañana:","skin_color"),("Color de los ojos:","eye_color"),("Año de nacimiento:","birth_year"),("Genero:","gender"),("Planeta de origen:","homeworld"))
+		listas=("Peliculas","Naves","Vehiculos","Especies")
 	elif tipo=="especies":
-		info={"url":"species","nombre":"name"}	
-		detalles=()
+		info={"url":"species","nombre":"name","titulo":"Especies","dato":"species"}	
+		detalle=()
 		listas=()
 	info["tipo"]=tipo
 	info["detalle"]=detalle
 	info["listas"]=listas
 	return info
+
+def conv_urls(urls,tipo):
+	new_url=[]
+	for url in urls:
+		path=url.split("/")
+		print(path)
+		new_url.append('/'+tipo+'/'+path[len(path)-2])
+	return new_url
+
 
 if __name__ == '__main__':
     app.run(debug=True)
