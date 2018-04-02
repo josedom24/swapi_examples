2ce92349ea3b6a3a81f1ccba8c978392eb85c8f6
josedom24 <josedom24@gmail.com>
2018-04-02 20:45:59 +0200

    swapi

--- flask2/app.py
+++ flask2/app.py
@@ -21,20 +21,27 @@ def detalle(tipo,id):
 	r=requests.get(URL_BASE+info["url"]+'/%d'%id)
 	if r.status_code == 200:
 		doc = r.json()
-		if tipo=="personajes":
+		if tipo=="personajes" or tipo=="especies":
 			info2=convtipo("planetas")
-			r=requests.get(doc["homeworld"])
-			if r.status_code == 200:
-				doc2=r.json()
-				nombre=doc2[info2["nombre"]]
-			url=conv_urls([doc["homeworld"]],"planetas")[0]
-			doc["homeworld"]='<a href="%s">%s</a>'%(url,nombre)
+			try:
+				r=requests.get(doc["homeworld"])
+				if r.status_code == 200:
+					doc2=r.json()
+					nombre=doc2[info2["nombre"]]
+				url=conv_urls([doc["homeworld"]],"planetas")[0]
+				doc["homeworld"]='<a href="%s">%s</a>'%(url,nombre)
+			except:
+				pass
 		return render_template("detalle.html",datos=doc,id=id,info=info)
 
 @app.route('/<string:tipo1>/<int:id>/<string:tipo2>')
 def datos(tipo1,id,tipo2):
 	info1=convtipo(tipo1)
 	info2=convtipo(tipo2)
+	if tipo1=="planetas" and tipo2=="personajes":
+		info2["dato"]="residents" 
+	elif tipo1=="especies" and tipo2=="personajes":
+		info2["dato"]="people"
 	r=requests.get(URL_BASE+info1["url"]+'/%d'%id)
 	if r.status_code == 200:
 		doc = r.json()  
@@ -57,8 +64,8 @@ def convtipo(tipo):
 
 	elif tipo=="planetas":
 		info={"url":"planets","nombre":"name","titulo":"Planetas","dato":"planets"}
-		detalle=("Periodo de rotación:":"rotation_period")
-		listas=()
+		detalle=(("Nombre:","name"),("Periodo de rotación:","rotation_period"),("Periodo orbital:,","orbital_period"),("Diámetro:,","diameter"),("Clima:,","climate"),("Gravedad:","gravity"),("Terreno:","terrain"),("Superficie con agua:","surface_water"),("Población:","population"))
+		listas=("Personajes","Peliculas")
 	elif tipo=="vehiculos":
 		info={"url":"vehicles","nombre":"name","titulo":"Vehículos","dato":"vehicles"}
 		detalle=()
@@ -69,12 +76,12 @@ def convtipo(tipo):
 		listas=()
 	elif tipo=="personajes":
 		info={"url":"people","nombre":"name","titulo":"Personajes","dato":"characters"}
-		detalle=(("Nombre:","name"),("Altura:","height"),("Peso:","mass"),("Color del pelo:","hair_color"),("Color de la mañana:","skin_color"),("Color de los ojos:","eye_color"),("Año de nacimiento:","birth_year"),("Genero:","gender"),("Planeta de origen:","homeworld"))
+		detalle=(("Nombre:","name"),("Altura:","height"),("Peso:","mass"),("Color del pelo:","hair_color"),("Color de piel:","skin_color"),("Color de los ojos:","eye_color"),("Año de nacimiento:","birth_year"),("Genero:","gender"),("Planeta de origen:","homeworld"))
 		listas=("Peliculas","Naves","Vehiculos","Especies")
 	elif tipo=="especies":
 		info={"url":"species","nombre":"name","titulo":"Especies","dato":"species"}	
-		detalle=()
-		listas=()
+		detalle=(("Nombre:","name"),("Clasificación","classification"),("Designación:","designation"),("Altura media:","average_height"),("Colores de piel:","skin_colors"),("Colores de pelo:","hair_colors"),("Colores de ojos:","eye_colors"),("Promedio de vida:","average_lifespan"),("Planeta de origen:","homeworld"),("Idioma:","language"))
+		listas=("Personajes","Peliculas")
 	info["tipo"]=tipo
 	info["detalle"]=detalle
 	info["listas"]=listas
